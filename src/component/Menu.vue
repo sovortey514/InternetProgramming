2ccd2d026cc4dd558f34e0a607588aea81ca17e7
sovortey <126035238+sovortey514@users.noreply.github.com>
2023-11-10 08:22:02 +0700

    TP01

---
+++ src/component/Menu.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="feature">
+        <div class="">
+            <h2>Featured Categories</h2>
+        </div>
+        <div class="listbar">
+            <div>
+                <h3>All</h3>
+            </div>
+            <div>
+                <h3>Milk&Dairies</h3>
+            </div>
+            <div>
+                <h3>Coffes&Teas</h3>
+            </div>
+            <div>
+                <h3>Pet Foods</h3>
+            </div>
+            <div>
+                <h3>Meats</h3>
+            </div>
+            <div>
+                <h3>Vegetables</h3>
+            </div>
+            <div>
+                <h3>Fruits</h3>
+            </div>
+        </div>
+    </div>
+</template>
+<<script>
+export default {
+    
+}
+</script>
+<style scoped>
+
+</style>
